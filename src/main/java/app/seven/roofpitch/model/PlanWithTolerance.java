2287cbb07e4e88e37ab5c1fe58493b031e1237ab
DOnn3r <nomenarasan@gmail.com>
2025-08-13 14:01:15 +0300

    feat: add model

---
+++ src/main/java/app/seven/roofpitch/model/PlanWithTolerance.java
@@ -0,0 +1,15 @@
+package app.seven.roofpitch.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Getter
+public class PlanWithTolerance {
+    private Plan plan;
+    private double tolerance;
+}
