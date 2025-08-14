f5c5eaf1ac2efbbe573320af1211481fc61c1ee7
Rakotondrasoa777 <rakotondrasoatojonandrianina5@gmail.com>
2025-08-14 14:25:19 +0300

    feat: add test of roofpitch

---
+++ src/test/java/app/seven/roofpitch/service/RoofPitchServiceTest.java
@@ -0,0 +1,33 @@
+package app.seven.roofpitch.service;
+
+import app.seven.roofpitch.model.Plan;
+import app.seven.roofpitch.model.Point;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class RoofPitchServiceTest {
+    private RoofPitchService roofPitchService = new RoofPitchService();
+
+    @Test
+    void roofPitchTest() {
+        List<Point> input = new ArrayList<>();
+
+        Plan plan1 = new Plan();
+        Plan plan2 = new Plan();
+        Plan plan3 = new Plan();
+
+        List<Plan> expected = new ArrayList<>();
+        expected.add(plan1);
+        expected.add(plan2);
+        expected.add(plan3);
+
+        List<Plan> actual = roofPitchService.getRoofPitch(input);
+
+        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(Plan.class, actual.getClass());
+    }
+}
