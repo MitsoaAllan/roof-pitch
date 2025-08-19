9bdf3d372a4a1edf6b71b32686ad2248d3fdb70a
DOnn3r <nomenarasan@gmail.com>
2025-08-19 21:31:20 +0300

    fix test

--- src/test/java/app/seven/roofpitch/tests/RoofPitchTest.java
+++ src/test/java/app/seven/roofpitch/tests/RoofPitchTest.java
@@ -18,7 +18,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(RoofPitchController.class)
-class RoofPitchControllerTest {
+class RoofPitchTest {
 
   @Autowired private MockMvc mockMvc;
 
@@ -32,7 +32,7 @@ class RoofPitchControllerTest {
     List<Point> points = List.of(new Point(0, 0, 0), new Point(1, 0, 1), new Point(0, 1, 1));
 
     RoofPlanResult mockResult = new RoofPlanResult();
-    mockResult.setSlopeDegrees(45.0);
+    mockResult.setSlopeDegrees(45.0); // Exemple de pente
 
     when(roofPitchService.detectRoofPlanes(points, 0.1, 0.2, 0.3)).thenReturn(List.of(mockResult));
 
