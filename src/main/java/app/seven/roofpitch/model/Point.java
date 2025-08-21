792b39fd472bc21312e87b093e6ba1c428c8d5c3
Rakotondrasoa777 <rakotondrasoatojonandrianina5@gmail.com>
2025-08-21 14:55:33 +0300

    feat: add math script for the constructor of plans

--- src/main/java/app/seven/roofpitch/model/Point.java
+++ src/main/java/app/seven/roofpitch/model/Point.java
@@ -12,11 +12,28 @@ public class Point {
   private Double y;
   private Double z;
 
-  public double distance(Point otherPoint) {
-    double dx = x - otherPoint.x;
-    double dy = y - otherPoint.y;
-    double dz = z - otherPoint.z;
-    return Math.sqrt(dx * dx + dy * dy + dz * dz);
+  //pour former un vecteur
+  public Point subtract(Point point) {
+    return new Point(x - point.x, y - point.y, z - point.z);
+  }
+
+  //mesure la position d'un point par rapport au plan
+  public double scalarProduct(Point p) {
+    return x * p.x + y * p.y + z * p.z;
+  }
+
+  // pour la construction du plan
+  public Point vectorProduct(Point p) {
+    return new Point(
+            y * p.z - z * p.y,
+            z * p.x - x * p.z,
+            x * p.y - y * p.x
+    );
+  }
+
+  //pour savoir si les point sont trop aligner
+  public double vectorNorm() {
+    return Math.sqrt(x*x + y*y + z*z);
   }
 
   @Override
