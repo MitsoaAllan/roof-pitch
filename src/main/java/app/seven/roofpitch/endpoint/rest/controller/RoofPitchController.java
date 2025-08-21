6dc7d52c2621da5045369769c7f983887c4929fe
Rakotondrasoa777 <rakotondrasoatojonandrianina5@gmail.com>
2025-08-21 20:34:54 +0300

    fix: customize controller

--- src/main/java/app/seven/roofpitch/endpoint/rest/controller/RoofPitchController.java
+++ src/main/java/app/seven/roofpitch/endpoint/rest/controller/RoofPitchController.java
@@ -1,18 +1,23 @@
 package app.seven.roofpitch.endpoint.rest.controller;
 
+import app.seven.roofpitch.model.PlanWithPente;
 import app.seven.roofpitch.model.Point;
 import app.seven.roofpitch.service.DetectePlanesService;
 import java.util.List;
+
+import app.seven.roofpitch.service.RoofPitchService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RoofPitchController {
 
-  private DetectePlanesService roofPitchService;
+  @Autowired
+  RoofPitchService service;
 
   @PostMapping("/roofpitch")
-  public List<List<Point>> roofpitch(
+  public List<PlanWithPente> roofpitch(
       @RequestBody List<Point> points, @RequestParam(required = false) Double tolerance) {
-    return roofPitchService.detectePlanes(points, tolerance);
+    return service.calculePentePlans(points, tolerance);
   }
 }
